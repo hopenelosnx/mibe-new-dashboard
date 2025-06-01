a68368381f53739731ee3b31402ae4c60ebca387
Hope Decardi-Nelson <79177876+blackbox24@users.noreply.github.com>
2025-06-01 19:14:58 +0000

    Update apiConfig.ts

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -5,7 +5,7 @@ interface Config {
 
 const token = localStorage.getItem("travelUserToken");
 const apiConfig: Config = {
-    baseurl: "http://mibes-new-backend-production-9834.up.railway.app/api/",
+    baseurl: "https://mibes-new-backend-production-9834.up.railway.app/api/",
     headers: {
         "Authorization": `Bearer ${token}`
     }
